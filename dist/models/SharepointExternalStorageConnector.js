336380a739073b8860917072d10157b4f1d11eea
Vincent Lamanna <vincent@crewdle.com>
2024-10-07 11:20:51 -0400

    Add a way to list changes to a drive (#1)
    * Add a way to list changes to a drive

--- dist/models/SharepointExternalStorageConnector.js
+++ dist/models/SharepointExternalStorageConnector.js
@@ -1,10 +1,11 @@
-import { FileStatus, ObjectKind } from '@crewdle/web-sdk-types';
+import { FileStatus, ObjectKind, StorageEventType } from '@crewdle/web-sdk-types';
 import { SharepointFile } from './SharepointFile';
 export class SharepointExternalStorageConnector {
     siteId;
     driveId;
     accessToken;
     refreshToken;
+    deltaLink;
     constructor(siteId, driveId, accessToken, refreshToken) {
         this.siteId = siteId;
         this.driveId = driveId;
@@ -19,26 +20,26 @@ export class SharepointExternalStorageConnector {
                     Authorization: `Bearer ${this.accessToken}`,
                 },
             });
-            if (response.status === 401 && retry) {
-                await this.handleRefreshToken();
-                return this.get(path, false);
-            }
             if (!response.ok) {
+                if (retry) {
+                    await this.handleRefreshToken();
+                    return this.get(path, false);
+                }
                 throw new Error(`Failed to get file: ${response.statusText}`);
             }
             const item = await response.json();
-            const fileResponse = await fetch(`https://graph.microsoft.com/v1.0/sites/${this.siteId}/drives/${this.driveId}/items/${item.id}/content`, {
-                headers: {
-                    Authorization: `Bearer ${this.accessToken}`,
-                },
-            });
+            const fileResponse = await fetch(item['@microsoft.graph.downloadUrl']);
             if (!fileResponse.ok) {
                 throw new Error(`Failed to get file: ${fileResponse.statusText}`);
             }
             const file = await fileResponse.blob();
-            return new SharepointFile(item.name, item.size, item.file.type, item.lastModifiedDateTime, file);
+            return new SharepointFile(item.name, item.size, item.file.mimeType, item.lastModifiedDateTime, file);
         }
         catch (error) {
+            if (retry) {
+                await this.handleRefreshToken();
+                return this.get(path, false);
+            }
             throw new Error(`Failed to get file: ${error.message}`);
         }
     }
@@ -55,22 +56,35 @@ export class SharepointExternalStorageConnector {
                     Authorization: `Bearer ${this.accessToken}`,
                 },
             });
-            if (response.status === 401 && retry) {
-                await this.handleRefreshToken();
-                return this.list(path, recursive, false);
-            }
             if (!response.ok) {
+                if (retry) {
+                    await this.handleRefreshToken();
+                    return this.list(path, recursive, false);
+                }
                 throw new Error(`Failed to list files: ${response.statusText}`);
             }
-            const data = await response.json();
-            for (const item of data.value) {
+            let data = await response.json();
+            const fetchItems = [...data.value];
+            while (data['@odata.nextLink']) {
+                const nextResponse = await fetch(data['@odata.nextLink'], {
+                    headers: {
+                        Authorization: `Bearer ${this.accessToken}`,
+                    },
+                });
+                if (!nextResponse.ok) {
+                    throw new Error(`Failed to list files: ${nextResponse.statusText}`);
+                }
+                data = await nextResponse.json();
+                fetchItems.push(...data.value);
+            }
+            for (const item of fetchItems) {
                 if (item.folder) {
                     items.push({
                         kind: ObjectKind.Folder,
                         name: item.name,
                         path,
-                        pathName: `${path}/${item.name}`,
-                        entries: recursive ? await this.list(`${path}/${item.name}`, recursive) : [],
+                        pathName: `${itemPath}/${item.name}`,
+                        entries: recursive ? await this.list(`${itemPath}/${item.name}`, recursive) : [],
                     });
                 }
                 else {
@@ -78,7 +92,7 @@ export class SharepointExternalStorageConnector {
                         kind: ObjectKind.File,
                         name: item.name,
                         path,
-                        pathName: `${path}/${item.name}`,
+                        pathName: `${itemPath}/${item.name}`,
                         size: item.size,
                         status: FileStatus.Synced,
                         type: item.file.mimeType,
@@ -88,10 +102,90 @@ export class SharepointExternalStorageConnector {
             return items;
         }
         catch (error) {
+            if (retry) {
+                await this.handleRefreshToken();
+                return this.list(path, recursive, false);
+            }
             throw new Error(`Failed to list files: ${error.message}`);
         }
     }
+    async listChanges(retry = true) {
+        const events = [];
+        try {
+            const url = this.deltaLink ? this.deltaLink : `https://graph.microsoft.com/v1.0/sites/${this.siteId}/drives/${this.driveId}/root/delta(token='latest')`;
+            const response = await fetch(url, {
+                headers: {
+                    Authorization: `Bearer ${this.accessToken}`,
+                },
+            });
+            if (!response.ok) {
+                if (retry) {
+                    await this.handleRefreshToken();
+                    return this.listChanges(false);
+                }
+                throw new Error(`Failed to list changes: ${response.statusText}`);
+            }
+            let data = await response.json();
+            events.push(...this.processChanges(data));
+            while (data['@odata.nextLink']) {
+                const nextResponse = await fetch(data['@odata.nextLink'], {
+                    headers: {
+                        Authorization: `Bearer ${this.accessToken}`,
+                    },
+                });
+                if (!nextResponse.ok) {
+                    throw new Error(`Failed to list changes: ${nextResponse.statusText}`);
+                }
+                data = await nextResponse.json();
+                events.push(...this.processChanges(data));
+            }
+            this.deltaLink = data['@odata.deltaLink'];
+        }
+        catch (error) {
+            if (retry) {
+                await this.handleRefreshToken();
+                return this.listChanges(false);
+            }
+            throw new Error(`Failed to list changes: ${error.message}`);
+        }
+        return events;
+    }
     async handleRefreshToken() {
         this.accessToken = await this.refreshToken();
+    }
+    processChanges(data) {
+        const events = [];
+        for (const item of data.value) {
+            const path = item.parentReference.path.replace(`/drives/${this.driveId}/root:`, '');
+            if (item.file) {
+                if (item.deleted) {
+                    events.push({
+                        event: StorageEventType.FileDelete,
+                        payload: {
+                            name: item.name,
+                            path: path,
+                            pathName: path + '/' + item.name,
+                        },
+                    });
+                }
+                else {
+                    events.push({
+                        event: StorageEventType.FileWrite,
+                        payload: {
+                            file: {
+                                kind: ObjectKind.File,
+                                path: item.parentReference.path,
+                                type: item.file.mimeType,
+                                size: item.size,
+                                name: item.name,
+                                pathName: path + '/' + item.name,
+                                status: FileStatus.Synced,
+                            },
+                        },
+                    });
+                }
+            }
+        }
+        return events;
     }
 }
--- src/models/SharepointExternalStorageConnector.ts
+++ src/models/SharepointExternalStorageConnector.ts
@@ -1,7 +1,9 @@
-import { FileStatus, IExternalStorageConnectionConnector, IFile, ObjectDescriptor, ObjectKind } from '@crewdle/web-sdk-types';
+import { FileStatus, IExternalStorageConnectionConnector, IFile, ObjectDescriptor, ObjectKind, StorageEvent, StorageEventType } from '@crewdle/web-sdk-types';
 import { SharepointFile } from './SharepointFile';
 
 export class SharepointExternalStorageConnector implements IExternalStorageConnectionConnector {
+  private deltaLink?: string;
+
   constructor(private siteId: string, private driveId: string, private accessToken: string, private refreshToken: () => Promise<string>) {}
 
   async get(path: string, retry = true): Promise<IFile> {
@@ -13,29 +15,30 @@ export class SharepointExternalStorageConnector implements IExternalStorageConne
         },
       });
 
-      if (response.status === 401 && retry) {
+      if (!response.ok) {
+        if (retry) {
         await this.handleRefreshToken();
         return this.get(path, false);
       }
 
-      if (!response.ok) {
         throw new Error(`Failed to get file: ${response.statusText}`);
       }
       const item = await response.json();
 
-      const fileResponse = await fetch(`https://graph.microsoft.com/v1.0/sites/${this.siteId}/drives/${this.driveId}/items/${item.id}/content`, {
-        headers: {
-          Authorization: `Bearer ${this.accessToken}`,
-        },
-      });
+      const fileResponse = await fetch(item['@microsoft.graph.downloadUrl']);
       
       if (!fileResponse.ok) {
         throw new Error(`Failed to get file: ${fileResponse.statusText}`);
       }
 
       const file = await fileResponse.blob();
-      return new SharepointFile(item.name, item.size, item.file.type, item.lastModifiedDateTime, file);
+      return new SharepointFile(item.name, item.size, item.file.mimeType, item.lastModifiedDateTime, file);
     } catch (error: any) {
+      if (retry) {
+        await this.handleRefreshToken();
+        return this.get(path, false);
+      }
+
       throw new Error(`Failed to get file: ${error.message}`);
     }
   }
@@ -54,31 +57,47 @@ export class SharepointExternalStorageConnector implements IExternalStorageConne
         },
       });
 
-      if (response.status === 401 && retry) {
+      if (!response.ok) {
+        if (retry) {
         await this.handleRefreshToken();
         return this.list(path, recursive, false);
       }
 
-      if (!response.ok) {
         throw new Error(`Failed to list files: ${response.statusText}`);
       }
 
-      const data = await response.json();
-      for (const item of data.value) {
+      let data = await response.json();
+      const fetchItems = [...data.value];
+      while (data['@odata.nextLink']) {
+        const nextResponse = await fetch(data['@odata.nextLink'], {
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+          },
+        });
+
+        if (!nextResponse.ok) {
+          throw new Error(`Failed to list files: ${nextResponse.statusText}`);
+        }
+
+        data = await nextResponse.json();
+        fetchItems.push(...data.value);
+      }
+
+      for (const item of fetchItems) {
         if (item.folder) {
           items.push({
             kind: ObjectKind.Folder,
             name: item.name,
             path,
-            pathName: `${path}/${item.name}`,
-            entries: recursive ? await this.list(`${path}/${item.name}`, recursive) : [],
+            pathName: `${itemPath}/${item.name}`,
+            entries: recursive ? await this.list(`${itemPath}/${item.name}`, recursive) : [],
           });
         } else {
           items.push({
             kind: ObjectKind.File,
             name: item.name,
             path,
-            pathName: `${path}/${item.name}`,
+            pathName: `${itemPath}/${item.name}`,
             size: item.size,
             status: FileStatus.Synced,
             type: item.file.mimeType,
@@ -88,11 +107,104 @@ export class SharepointExternalStorageConnector implements IExternalStorageConne
 
       return items;
     } catch (error: any) {
+      if (retry) {
+        await this.handleRefreshToken();
+        return this.list(path, recursive, false);
+      }
+
       throw new Error(`Failed to list files: ${error.message}`);
     }
   }
 
+  async listChanges(retry = true): Promise<StorageEvent[]> {
+    const events: StorageEvent[] = [];
+
+    try {
+      const url = this.deltaLink ? this.deltaLink : `https://graph.microsoft.com/v1.0/sites/${this.siteId}/drives/${this.driveId}/root/delta(token='latest')`;
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        if (retry) {
+        await this.handleRefreshToken();
+        return this.listChanges(false);
+      }
+
+        throw new Error(`Failed to list changes: ${response.statusText}`);
+      }
+
+      let data = await response.json();
+      events.push(...this.processChanges(data));
+
+      while (data['@odata.nextLink']) {
+        const nextResponse = await fetch(data['@odata.nextLink'], {
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+          },
+        });
+
+        if (!nextResponse.ok) {
+          throw new Error(`Failed to list changes: ${nextResponse.statusText}`);
+        }
+
+        data = await nextResponse.json();
+        events.push(...this.processChanges(data));
+      }
+
+      this.deltaLink = data['@odata.deltaLink'];
+    } catch (error: any) {
+      if (retry) {
+        await this.handleRefreshToken();
+        return this.listChanges(false);
+      }
+
+      throw new Error(`Failed to list changes: ${error.message}`);
+    }
+
+    return events;
+  }
+
   private async handleRefreshToken() {
     this.accessToken = await this.refreshToken();
+  }
+
+  private processChanges(data: any): StorageEvent[] {
+    const events: StorageEvent[] = [];
+
+    for (const item of data.value) {
+      const path = item.parentReference.path.replace(`/drives/${this.driveId}/root:`, '');
+      if (item.file) {
+        if (item.deleted) {
+          events.push({
+            event: StorageEventType.FileDelete,
+            payload: {
+              name: item.name,
+              path: path,
+              pathName: path + '/' + item.name,
+            },
+          });
+        } else {
+          events.push({
+            event: StorageEventType.FileWrite,
+            payload: {
+              file: {
+                kind: ObjectKind.File,
+                path: item.parentReference.path,
+                type: item.file.mimeType,
+                size: item.size,
+                name: item.name,
+                pathName: path + '/' + item.name,
+                status: FileStatus.Synced,
+              },
+            },
+          });
+        }
+      }
+    }
+
+    return events;
   }
 }
